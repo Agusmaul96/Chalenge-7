2035b8ba23111dc5ce3e4ec5f65350578d270ff2
agusmaul96 <agusmaulanaa96@gmail.com>
2022-04-22 08:42:03 +0700

    commit

--- routes/biodata.js
+++ routes/biodata.js
@@ -7,10 +7,5 @@ const apiRestrict = require("./../middlewares/apiRestrict");
 router.get("/biodata", apiRestrict, biodata.getBiodata);
 router.get("/biodata/add", biodata.addBiodata);
 router.post("/biodata/post", biodata.newBiodata);
-// UPDATE DATA
-router.get("/biodata/edit/:id", biodata.editBiodata);
 
-router.post("/biodata/update", biodata.updateBiodata);
-// delete data
-router.get("/biodata/delete/:id", biodata.deleteBiodata);
 module.exports = router;
